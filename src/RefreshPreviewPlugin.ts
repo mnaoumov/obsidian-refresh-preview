6879a5936fb36a6a8722d9bbb09abc8a8fc57e13
Michael Naumov <mnaoumov@gmail.com>
2024-07-04 19:22:25 -0600

    addRefreshPreviewButton

--- src/RefreshPreviewPlugin.ts
+++ src/RefreshPreviewPlugin.ts
@@ -1,6 +1,7 @@
 import {
   MarkdownView,
-  Plugin
+  Plugin,
+  setIcon
 } from "obsidian";
 import RefreshPreviewPluginSettings from "./RefreshPreviewPluginSettings.ts";
 import RefreshPreviewPluginSettingsTab from "./RefreshPreviewPluginSettingsTab.ts";
@@ -19,8 +20,14 @@ export default class RefreshPreviewPlugin extends Plugin {
     this.addCommand({
       id: "refresh-preview",
       name: "Refresh Preview",
-      checkCallback: this.refreshPreview.bind(this),
+      checkCallback: this.refreshPreview.bind(this)
     });
+
+    this.registerEvent(
+      this.app.workspace.on("layout-change", () => {
+        this.addRefreshPreviewButton();
+      })
+    );
   }
 
   public async saveSettings(newSettings: RefreshPreviewPluginSettings): Promise<void> {
@@ -29,6 +36,7 @@ export default class RefreshPreviewPlugin extends Plugin {
   }
 
   private async onLayoutReady(): Promise<void> {
+    this.addRefreshPreviewButton();
   }
 
   private async loadSettings(): Promise<void> {
@@ -45,5 +53,45 @@ export default class RefreshPreviewPlugin extends Plugin {
       view.previewMode.rerender(true);
     }
     return true;
+  }
+
+  private addRefreshPreviewButton(): void {
+    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!view) {
+      return;
+    }
+
+    const actionsContainer = view.containerEl.querySelector(".view-header .view-actions");
+
+    if (!actionsContainer) {
+      return;
+    }
+
+    let refreshPreviewButton = actionsContainer.querySelector<HTMLButtonElement>(".refresh-preview-button");
+
+    if (view.getMode() !== "preview") {
+      if (refreshPreviewButton) {
+        actionsContainer.removeChild(refreshPreviewButton);
+      }
+      return;
+    }
+
+    if (refreshPreviewButton) {
+      return;
+    }
+
+    refreshPreviewButton = createEl("button", {
+      cls: "refresh-preview-button clickable-icon view-action",
+      onclick: () => this.refreshPreview(false)
+    });
+    setIcon(refreshPreviewButton, "refresh-cw");
+
+    actionsContainer.prepend(refreshPreviewButton)
+
+    this.register(() => {
+      if (actionsContainer.contains(refreshPreviewButton)) {
+        actionsContainer.removeChild(refreshPreviewButton);
+      }
+    });
   }
 }
