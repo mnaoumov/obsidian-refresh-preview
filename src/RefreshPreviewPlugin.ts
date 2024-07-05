c563ec1c6521fcf46ab9e27e47ffc9a06a4a3429
Michael Naumov <mnaoumov@gmail.com>
2024-07-04 19:23:11 -0600

    Remove settings

--- src/RefreshPreviewPlugin.ts
+++ src/RefreshPreviewPlugin.ts
@@ -3,19 +3,9 @@ import {
   Plugin,
   setIcon
 } from "obsidian";
-import RefreshPreviewPluginSettings from "./RefreshPreviewPluginSettings.ts";
-import RefreshPreviewPluginSettingsTab from "./RefreshPreviewPluginSettingsTab.ts";
 
 export default class RefreshPreviewPlugin extends Plugin {
-  private _settings!: RefreshPreviewPluginSettings;
-
-  public get settings(): RefreshPreviewPluginSettings {
-    return RefreshPreviewPluginSettings.clone(this._settings);
-  }
-
   public override async onload(): Promise<void> {
-    await this.loadSettings();
-    this.addSettingTab(new RefreshPreviewPluginSettingsTab(this));
     this.app.workspace.onLayoutReady(this.onLayoutReady.bind(this));
     this.addCommand({
       id: "refresh-preview",
@@ -30,17 +20,8 @@ export default class RefreshPreviewPlugin extends Plugin {
     );
   }
 
-  public async saveSettings(newSettings: RefreshPreviewPluginSettings): Promise<void> {
-    this._settings = RefreshPreviewPluginSettings.clone(newSettings);
-    await this.saveData(this._settings);
-  }
-
   private async onLayoutReady(): Promise<void> {
     this.addRefreshPreviewButton();
-  }
-
-  private async loadSettings(): Promise<void> {
-    this._settings = RefreshPreviewPluginSettings.load(await this.loadData());
   }
 
   private refreshPreview(checking: boolean): boolean {
