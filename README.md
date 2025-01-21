7eaf08c699d8365617555e78de696476e3e3adf5
Michael Naumov <mnaoumov@gmail.com>
2025-01-20 20:52:45 -0600

    New template

--- README.md
+++ README.md
@@ -27,6 +27,18 @@ The plugin allows to configure auto refresh within a given time interval.
 - `Refresh Preview` is available on [the official Community Plugins repository](https://obsidian.md/plugins?id=refresh-preview).
 - Beta releases can be installed through [BRAT](https://obsidian.md/plugins?id=obsidian42-brat).
 
+## Debugging
+
+By default, debug messages for this plugin are hidden.
+
+To show them, run the following command:
+
+```js
+window.DEBUG.enable('refresh-preview');
+```
+
+For more details, refer to the [documentation](https://github.com/mnaoumov/obsidian-dev-utils?tab=readme-ov-file#debugging).
+
 ## Support
 
 <a href="https://www.buymeacoffee.com/mnaoumov" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;"></a>
--- src/RefreshPreviewPluginSettingsTab.ts
+++ src/RefreshPreviewPluginSettingsTab.ts
@@ -1,6 +1,5 @@
 import { Setting } from 'obsidian';
 import { PluginSettingsTabBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginSettingsTabBase';
-import { extend } from 'obsidian-dev-utils/obsidian/Plugin/ValueComponent';
 
 import type { RefreshPreviewPlugin } from './RefreshPreviewPlugin.ts';
 
@@ -10,13 +9,13 @@ export class RefreshPreviewPluginSettingsTab extends PluginSettingsTabBase<Refre
 
     new Setting(this.containerEl)
       .setName('Auto refresh on file change')
-      .addToggle((toggle) => extend(toggle).bind(this.plugin, 'autoRefreshOnFileChange'));
+      .addToggle((toggle) => this.bind(toggle, 'autoRefreshOnFileChange'));
 
     new Setting(this.containerEl)
       .setName('Auto refresh interval (seconds)')
       .setDesc('Set to 0 to disable auto refresh')
       .addText((text) => {
-        extend(text).bind(this.plugin, 'autoRefreshIntervalInSeconds', {
+        this.bind(text, 'autoRefreshIntervalInSeconds', {
           componentToPluginSettingsValueConverter: (uiValue: string) => parseInt(uiValue, 10),
           pluginSettingsToComponentValueConverter: (pluginSettingsValue: number) => pluginSettingsValue.toString(),
           valueValidator() {
@@ -26,7 +25,7 @@ export class RefreshPreviewPluginSettingsTab extends PluginSettingsTabBase<Refre
             if (text.inputEl.valueAsNumber < 0) {
               return 'Value cannot be negative';
             }
-            return null;
+            return;
           }
         });
         text.inputEl.type = 'number';
