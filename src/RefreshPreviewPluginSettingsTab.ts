4ac6c471bc195413a1fdd6b661962914673d9c0b
Michael Naumov <mnaoumov@gmail.com>
2025-02-06 19:51:33 -0600

    Switch to number component

--- src/RefreshPreviewPluginSettingsTab.ts
+++ src/RefreshPreviewPluginSettingsTab.ts
@@ -1,5 +1,6 @@
 import { Setting } from 'obsidian';
 import { PluginSettingsTabBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginSettingsTabBase';
+import { SettingEx } from 'obsidian-dev-utils/obsidian/SettingEx';
 
 import type { RefreshPreviewPlugin } from './RefreshPreviewPlugin.ts';
 
@@ -11,26 +12,12 @@ export class RefreshPreviewPluginSettingsTab extends PluginSettingsTabBase<Refre
       .setName('Auto refresh on file change')
       .addToggle((toggle) => this.bind(toggle, 'autoRefreshOnFileChange'));
 
-    new Setting(this.containerEl)
+    new SettingEx(this.containerEl)
       .setName('Auto refresh interval (seconds)')
       .setDesc('Set to 0 to disable auto refresh')
-      .addText((text) => {
-        this.bind(text, 'autoRefreshIntervalInSeconds', {
-          componentToPluginSettingsValueConverter: (uiValue: string) => parseInt(uiValue, 10),
-          pluginSettingsToComponentValueConverter: (pluginSettingsValue: number) => pluginSettingsValue.toString(),
-          // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-          valueValidator(uiValue: string): string | void {
-            const num = parseInt(uiValue, 10);
-            if (isNaN(num)) {
-              return 'Please enter a numeric value';
-            }
-            if (num < 0) {
-              return 'Value cannot be negative';
-            }
-          }
-        });
-        text.inputEl.type = 'number';
-        text.inputEl.min = '0';
+      .addNumber((number) => {
+        this.bind(number, 'autoRefreshIntervalInSeconds')
+          .setMin(0);
       });
   }
 }
